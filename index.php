a3fcfadc23cd21e908bc6f372438f942b8455c17
rismapradnyadari <79454023+rismapradnyadari@users.noreply.github.com>
2021-02-25 22:45:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Tugas Risma</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+	<link rel="canonical" href="http://informatika.undiksha.ac.id/">
+	<meta property="og:locale" content="en_US">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+	<meta property="og:description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+	<meta property="og:url" content="http://informatika.undiksha.ac.id/">
+	<meta property="og:site_name" content="Manajemen Informatika Universitas Pendidikan Ganesha">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="jumbotron text-center">
+  <h1>PORTAL BERITA MASA KINI</h1>
+  <p>Ni Luh Risma Pradnyadari</p> 
+  <p>25/02/2021</p>
+  <img src="media\news.jpg">
+</div>
+  
+<div class="container">
+  <div class="row">
+  <div class="container">  
+  <div class="col-sm-4">
+      <h3>Sosial</h3>
+    </div>
+    <div class="col-sm-4">
+      <h3>Olahraga</h3>
+    </div>
+    <div class="col-sm-4">
+      <h3>Ekonomi</h3>        
+    </div>
+    <div class="col-sm-4">
+      <h3>Kuliner</h3>        
+    </div>
+    <div class="col-sm-4">
+      <h3>Hiburan</h3>        
+    </div>
+    <div class="col-sm-4">
+      <h3>Kesehatan</h3>        
+    </div>
+  </div>
+</div>
+    </body>
+</html>
