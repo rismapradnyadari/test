09f13f795aad0a5ba5bc3b140850f8ddb06f82be
rismapradnyadari <79454023+rismapradnyadari@users.noreply.github.com>
2021-03-07 12:49:57 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,64 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    <title>Tugas Risma</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
-	<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
-	<link rel="canonical" href="http://informatika.undiksha.ac.id/">
-	<meta property="og:locale" content="en_US">
-	<meta property="og:type" content="website">
-	<meta property="og:title" content="Manajemen Informatika Universitas Pendidikan Ganesha">
-	<meta property="og:description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
-	<meta property="og:url" content="http://informatika.undiksha.ac.id/">
-	<meta property="og:site_name" content="Manajemen Informatika Universitas Pendidikan Ganesha">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="assets/css/style.css">
-    </head>
-    <body>
-    <!DOCTYPE html>
-<html lang="en">
+
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Berita</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 </head>
+
 <body>
-  
-<div class="jumbotron text-center">
-  <h1>PORTAL BERITA MASA KINI</h1>
-  <p>Ni Luh Risma Pradnyadari</p> 
-  <p>25/02/2021</p>
-  <img src="media\news.jpg">
-</div>
-  
-<div class="container">
-  <div class="row">
-  <div class="container">  
-  <div class="col-sm-4">
-      <h3>Sosial</h3>
-    </div>
-    <div class="col-sm-4">
-      <h3>Olahraga</h3>
-    </div>
-    <div class="col-sm-4">
-      <h3>Ekonomi</h3>        
-    </div>
-    <div class="col-sm-4">
-      <h3>Kuliner</h3>        
-    </div>
-    <div class="col-sm-4">
-      <h3>Hiburan</h3>        
-    </div>
-    <div class="col-sm-4">
-      <h3>Kesehatan</h3>        
-    </div>
-  </div>
-</div>
-    </body>
+    <section class="header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+                <a class="navbar-brand" href="#">Portal Berita Masa Kini</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto text-right">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Alamat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Bantuan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="media/form.html">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+
+    </section>
+    <section class="slider">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/img/img (1).jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/img/img (2).jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/img/img (3).jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+    </section>
+
+    <section class="us">
+        <div class="container">
+            <h2 class="tittle"> Deskripsi </h2>
+            <p class="subtittle">Teks Berita adalah teks yang melaporkan kejadian, 
+                peristiwa atau infomasi mengenai sesuatu yang telah atau sedang terjadi. 
+                Penyampaian berita ini bisa dilakukan secara lisan yang sering kita dengar
+                 dan lihat di televisi, dan secara tulisan yang dapat kit abaca di media cetak.
+                 engan membaca atau mendengarkan teks berita, kita dapat memperoleh berbagai informasi mengenai suatu hal. 
+                 Bertambahnya informasi berarti bertambah pula wawasan kita, 
+                 sehingga kita dapat berfikir secara menyeluruh, efektif, kreatif dan kritis terhadap suatu masalah yang terjadi di sekitar kita. 
+            </p>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="bx-us">
+                        <h3> Fashion </h3>
+                        <p> Secara khusus bisa dikatakan Fashion adalah 
+                            gaya berpakaian yang dipakai setiap hari oleh seseorang, 
+                            baik itu kehidupan sehari-hari dirumah maupun acara tertentu 
+                            dengan tujuan untuk menunjang penampilan.
+                            Berbagai tipe dari gaya fashion yang akan dibahas 
+                            dapat mengungkapkan tema penampilan kita pada umumnya.
+</p>
+                        <img src="assets/img/imgs (1).jpg">
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+                    <div class="bx-us">
+                        <h3> Sport </h3>
+                        <p> Berita olahraga tentu tak bisa begitu saja dilewatkan, terlebih bagi pecinta jenis sport tertentu.
+                             Tak hanya mengenai pertandingan saja, 
+                            bahkan terkadang kehidupan sosok legendaris dalam dunia olahraga tak luput dikulik oleh media.</p>
+                        <img src="assets/img/imgs (2).jpg">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="bx-us">
+                        <h3> Food </h3>
+                        <p> Ada beragam inovasi kuliner di tahun ini, salah satunya yang sedang hits yaitu sandwich toast. Saat ini sandwich toast menjadi salah satu kuliner yang digemari masyarakat Ibu Kota.
+                            Selain karena rasanya yang enak, kudapan satu ini juga terbilang mengenyangkan karena diisi dengan berbagai macam pilihan topping. 
+                            Biasanya, kedai kopi atau coffee shop dan juga kedai minuman seperti bubble tea juga menyediakan sandwich toast sebagai menunya</p>
+                        <img src="assets/img/imgs (3).jpg">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section>
+        <div class="container text-center">
+        </div>
+    </section>
+
+</body>
+
 </html>
